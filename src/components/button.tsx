3b634aa5e08a55cf5f7032887ccda48ff7d2db45
Mijura <miodragvilotijevic@gmail.com>
2025-01-14 18:15:35 +0100

    button onClick prop

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,17 +1,19 @@
 import React from "react"
 import { HTMLMotionProps, motion } from "framer-motion"
 
-interface MotionButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-	children: React.ReactNode;
-	className?: string;
-  }
+interface ButtonProps extends HTMLMotionProps<"button"> {
+	children: React.ReactNode,
+	onClick?: () => void
+	className?: string
+}
 
-const Button: React.FC<MotionButtonProps> = ({ children, className = "" }) => {
+const Button: React.FC<ButtonProps> = ({ children, className = "", onClick = () => {} }) => {
 	return (
 		<motion.button
 			className={`px-2 py-1 bg-clickable text-surface1 rounded-md hover:bg-hover text-sm transition-colors duration-200 ${className}`}
 			whileHover={{ scale: 1.05 }}
 			whileTap={{ scale: 0.95 }}
+			onClick={onClick}
 		>
 			{children}
 		</motion.button>
