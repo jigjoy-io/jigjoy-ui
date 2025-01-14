20ba7d49ac777f3c2a374eda4b052c38d2631c60
Mijura <miodragvilotijevic@gmail.com>
2025-01-14 16:58:14 +0100

    revert react-dom to external dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup"
 export default defineConfig({
 	format: ["cjs", "esm"],
 	entry: ["src/index.ts"],
-	external: ["react", "framer-motion"],
+	external: ["react", "react-dom", "framer-motion"],
 	dts: true,
 	clean: true,
 })
