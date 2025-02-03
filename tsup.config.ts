5847b766659f1aca46d8a7e400063598ba9b81ad
Mijura <miodragvilotijevic@gmail.com>
2025-02-03 12:51:41 +0100

    export thailwind config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "tsup"
+import fs from "fs"
+import path from "path"
 
 export default defineConfig({
 	format: ["cjs", "esm"],
 	entry: ["src/index.ts"],
 	external: ["react", "react-dom", "framer-motion"],
 	target: "es2019",
+	dts: true,
+	outDir: "dist",
 	clean: true,
+	esbuildOptions() {
+		const src = path.resolve("src/index.css")
+		const dest = path.resolve("dist/theme.css")
+		fs.copyFileSync(src, dest)
+	},
 })
